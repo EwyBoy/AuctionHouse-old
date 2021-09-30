9ea83080bb29fc8e4de122c87ec81456f1d934fa
Ewy <ewyboy@gmail.com>
2021-09-30 20:47:31 +0200

    Init()

---
+++ src/main/java/com/ewyboy/auctionhouse/AuctionHouse.java
@@ -0,0 +1,16 @@
+package com.ewyboy.auctionhouse;
+
+import net.minecraftforge.fml.common.Mod;
+
+import static com.ewyboy.auctionhouse.AuctionHouse.MOD_ID;
+
+@Mod(MOD_ID)
+public class AuctionHouse {
+
+    public static final String MOD_ID = "auctionhouse";
+
+    public AuctionHouse() {
+
+    }
+
+}
